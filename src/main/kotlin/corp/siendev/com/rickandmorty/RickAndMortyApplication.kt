53cb9b526625a6dd0705c1fc6bb67c5ae0114459
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-06-15 18:17:53 +0300

    initial commit

---
+++ src/main/kotlin/corp/siendev/com/rickandmorty/RickAndMortyApplication.kt
@@ -0,0 +1,11 @@
+package corp.siendev.com.rickandmorty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RickAndMortyApplication
+
+fun main(args: Array<String>) {
+    runApplication<RickAndMortyApplication>(*args)
+}
---
+++ src/test/kotlin/corp/siendev/com/rickandmorty/RickAndMortyApplicationTests.kt
@@ -0,0 +1,13 @@
+package corp.siendev.com.rickandmorty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RickAndMortyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
