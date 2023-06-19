b91876ef2edd9ea42223a4199091c33b537cfdef
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-06-19 22:26:50 +0300

    restructured the project

--- Backend/src/main/kotlin/corp/siendev/com/rickandmorty/RickAndMortyApplication.kt
+++ Backend/src/main/kotlin/corp/siendev/com/rickandmorty/RickAndMortyApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class RickAndMortyApplication
 
 fun main(args: Array<String>) {
-    runApplication<RickAndMortyApplication>(*args)
+	runApplication<RickAndMortyApplication>(*args)
 }
--- Backend/src/test/kotlin/corp/siendev/com/rickandmorty/RickAndMortyApplicationTests.kt
+++ Backend/src/test/kotlin/corp/siendev/com/rickandmorty/RickAndMortyApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class RickAndMortyApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+	@Test
+	fun contextLoads() {
+	}
 
 }
