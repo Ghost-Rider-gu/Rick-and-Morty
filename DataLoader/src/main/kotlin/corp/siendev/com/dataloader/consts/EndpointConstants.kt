5ca7ce810dc402e6bd4a94fef80d5afa7419c584
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-06-23 21:40:14 +0300

    added some common constants

---
+++ DataLoader/src/main/kotlin/corp/siendev/com/dataloader/consts/EndpointConstants.kt
@@ -0,0 +1,12 @@
+package corp.siendev.com.dataloader.consts
+
+object EndpointConstants {
+    const val MAIN_ENDPOINT = "https://rickandmortyapi.com/api"
+    const val CHARACTER_PATH = "character"
+    const val LOCATION_PATH = "location"
+    const val EPISODE_PATH = "episode"
+
+    const val CHARACTER_COUNT = 826
+    const val LOCATION_COUNT = 126
+    const val EPISODE_COUNT = 51
+}
