7ae240665401afd9976adbc276aed51e6fbc92bc
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-06-27 21:56:37 +0300

    added SqlWriterConstants

---
+++ DataLoader/src/main/kotlin/corp/siendev/com/dataloader/consts/SqlWriterConstants.kt
@@ -0,0 +1,11 @@
+package corp.siendev.com.dataloader.consts
+
+object SqlWriterConstants {
+    const val CHARACTER_SQL_FILE_NAME = "data/characters_data.sql"
+    const val EPISODE_SQL_FILE_NAME = "data/episodes_data.sql"
+    const val LOCATION_SQL_FILE_NAME = "data/locations_data.sql"
+
+    const val CHARACTER_TABLE_NAME = "character"
+    const val EPISODE_TABLE_NAME = "episode"
+    const val LOCATION_TABLE_NAME = "location"
+}
