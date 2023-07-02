596838a3041ea3f35e26db10505f54772604191b
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-07-02 21:42:00 +0300

    DataLoader updates

--- DataLoader/src/main/kotlin/corp/siendev/com/dataloader/DataLoaderClient.kt
+++ DataLoader/src/main/kotlin/corp/siendev/com/dataloader/DataLoaderClient.kt
@@ -1,6 +1,7 @@
 package corp.siendev.com.dataloader
 
-import com.google.gson.Gson
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
 import corp.siendev.com.dataloader.consts.EndpointConstants
 import corp.siendev.com.dataloader.consts.SqlWriterConstants
 import corp.siendev.com.dataloader.model.Character
@@ -10,14 +11,16 @@ import corp.siendev.com.dataloader.service.EntityLoadService
 import corp.siendev.com.dataloader.service.SqlWriterService
 
 class DataLoaderClient(private val entityLoadService: EntityLoadService, private val sqlWriterService: SqlWriterService) {
-    private val gson = Gson()
+    private val gson = GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .create()
 
     fun loadCharacters() {
         println("Starting to load characters ...")
         for (id in 1..EndpointConstants.CHARACTER_COUNT) {
             val characterResponse = entityLoadService.getCharacterById(id)
             val character = gson.fromJson(characterResponse, Character::class.java)
-            sqlWriterService.entityToSqlFile(character.toString(), SqlWriterConstants.CHARACTER_SQL_FILE_NAME)
+            sqlWriterService.entityToSqlFile(character.toString(), getFullPath(SqlWriterConstants.CHARACTER_SQL_FILE_NAME))
             println("Character (${character.name}) with id=$id successfully load")
         }
         println("== Finished with characters ==")
@@ -28,7 +31,7 @@ class DataLoaderClient(private val entityLoadService: EntityLoadService, private
         for (id in 1..EndpointConstants.EPISODE_COUNT) {
             val episodeResponse = entityLoadService.getEpisodeById(id)
             val episode = gson.fromJson(episodeResponse, Episode::class.java)
-            sqlWriterService.entityToSqlFile(episode.toString(), SqlWriterConstants.EPISODE_SQL_FILE_NAME)
+            sqlWriterService.entityToSqlFile(episode.toString(), getFullPath(SqlWriterConstants.EPISODE_SQL_FILE_NAME))
             println("Episode (${episode.name}) with id=$id successfully load")
         }
         println("== Finished with episodes ==")
@@ -39,9 +42,13 @@ class DataLoaderClient(private val entityLoadService: EntityLoadService, private
         for (id in 1..EndpointConstants.LOCATION_COUNT) {
             val locationResponse = entityLoadService.getLocationById(id)
             val location = gson.fromJson(locationResponse, Location::class.java)
-            sqlWriterService.entityToSqlFile(location.toString(), SqlWriterConstants.LOCATION_SQL_FILE_NAME)
+            sqlWriterService.entityToSqlFile(location.toString(), getFullPath(SqlWriterConstants.LOCATION_SQL_FILE_NAME))
             println("Location (${location.name}) with id=$id successfully load")
         }
         println("== Finished with locations ==")
+    }
+
+    private fun getFullPath(fileName: String): String {
+        return "${SqlWriterConstants.FOLDER_FOR_SQL_FILES}/$fileName"
     }
 }
--- DataLoader/src/main/kotlin/corp/siendev/com/dataloader/service/SqlWriterServiceImpl.kt
+++ DataLoader/src/main/kotlin/corp/siendev/com/dataloader/service/SqlWriterServiceImpl.kt
@@ -1,14 +1,27 @@
 package corp.siendev.com.dataloader.service
 
+import corp.siendev.com.dataloader.consts.SqlWriterConstants
 import okio.IOException
-import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class SqlWriterServiceImpl: SqlWriterService {
+    init {
+        try {
+            if (!Files.isDirectory(Paths.get(SqlWriterConstants.FOLDER_FOR_SQL_FILES))) {
+                Files.createDirectory(Paths.get(SqlWriterConstants.FOLDER_FOR_SQL_FILES))
+            }
+        } catch (ex:IOException) {
+            println("Couldn't create a directory: ${ex.message}")
+        }
+    }
+
     override fun entityToSqlFile(entityData: String, fileName: String) {
         try {
-           File(fileName).printWriter().use {
-               writer -> writer.write(entityData)
-           }
+            FileOutputStream(fileName, true).bufferedWriter().use {
+                it.append(entityData).appendLine()
+            }
         } catch (ex: IOException) {
             println("Couldn't write data into the file: ${ex.message}")
             return
